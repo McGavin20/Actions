23e11958fe1786090c14de6bdcab713ba5c3fc8f
Sharma <smuridzo20@gmail.com>
2023-09-06 14:54:23 +0200

    Added: Weekly View

---
+++ Actions/Views/WeeklyView.swift
@@ -0,0 +1,20 @@
+//
+//  WeeklyView.swift
+//  Actions
+//
+//  Created by Sharma on 06/09/2023.
+//
+
+import SwiftUI
+
+struct WeeklyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeeklyView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeeklyView()
+    }
+}
